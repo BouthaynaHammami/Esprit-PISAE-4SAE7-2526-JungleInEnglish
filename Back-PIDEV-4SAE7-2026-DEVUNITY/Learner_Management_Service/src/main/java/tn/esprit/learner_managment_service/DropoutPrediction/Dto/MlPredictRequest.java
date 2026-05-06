53bcdb90db29d409f2a41e4ce7e89b789828d962
islem <islem.raissi@etudiant-isi.utm.tn>
2026-05-06 23:12:30 +0200

    fix tests

--- Back-PIDEV-4SAE7-2026-DEVUNITY/Learner_Management_Service/src/main/java/tn/esprit/learner_managment_service/DropoutPrediction/Dto/MlPredictRequest.java
+++ Back-PIDEV-4SAE7-2026-DEVUNITY/Learner_Management_Service/src/main/java/tn/esprit/learner_managment_service/DropoutPrediction/Dto/MlPredictRequest.java
@@ -1,6 +1,5 @@
 package tn.esprit.learner_managment_service.DropoutPrediction.Dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
@@ -8,6 +7,5 @@ import lombok.experimental.SuperBuilder;
 @Getter
 @SuperBuilder
 @NoArgsConstructor
-@AllArgsConstructor
 public class MlPredictRequest extends DropoutFormBaseDto {
 }
